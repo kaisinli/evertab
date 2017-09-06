b2c7e506c575eb4bfbdfb701a5302a76f60daddf
Kaisin Li <kaisinli12@gmail.com>
2017-09-06 15:08:35 -0400

    api hook up

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,10 @@
 import authLink from '../../secrets.js'
-var storage = window.localStorage;
 
 $(document).ready(function () {
     var handleOne = storage.getItem('handleOne');
     var handleTwo = storage.getItem('handleTwo');
 
     $('form')
-        .attr('method', 'post')
-        .attr('action', authLink)
         .submit(function (e) {
             var newHandleOne = $('#handle-one').val();
             storage.setItem('handleOne', newHandleOne);
--- public/js/popup.js
+++ public/js/popup.js
@@ -1,30 +1,38 @@
-import authLink from '../../secrets.js';
+import INSTAGRAM_CLIENT_ID from '../../secrets.js';
+var redirectUri = 'https://www.instagram.com/evertabextension/';
+var authLink = `https://api.instagram.com/oauth/authorize/?client_id=${INSTAGRAM_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=token`;
+
 var storage = window.localStorage;
-var Promise = require('bluebird');
 
 $(document).ready(function () {
     var handleOne = storage.getItem('handleOne');
     var handleTwo = storage.getItem('handleTwo');
     var accessToken = storage.getItem('accessToken');
-    var urlAccessToken = '';
 
     if (accessToken === null) {
-        //query AT in url
-        chrome.tabs.query({ active: true, currentWindow: true }, function callback(tabs) {
-            var currentTab = tabs[0];
-            console.log(currentTab);
-        })
+        chrome.tabs.query({ active: true, currentWindow: true },
+            function isATInUrl(tabs) {
+                var currentTabUrl = tabs[0].url;
+                var ATFragmentInUrl = redirectUri + '#access_token=';
+                var domainPathAndFrag = currentTabUrl.slice(0, 57);
 
-        if (urlAccessToken = '') {
-            $("#step-two").remove();
-            $("#new-handle-form").remove();
-            $('a').attr('href', authLink);
-
-        } else {
-            $("#step-one").remove();
-            $('#step-two').on('click', function () { })
-        }
-
+                if (domainPathAndFrag !== ATFragmentInUrl) {
+                    $("#step-two").remove();
+                    $("#new-handle-form").remove();
+                    $('a').attr('href', authLink);
+                } else {
+                    $("#step-one").remove();
+                    $('form')
+                        .submit(function (e) {
+                            var newHandleOne = $('#handle-one').val();
+                            storage.setItem('handleOne', newHandleOne);
+                            var newHandleTwo = $('#handle-two').val();
+                            storage.setItem('handleTwo', newHandleTwo);
+                            var accessToken = currentTabUrl.slice(57, currentTabUrl.length);
+                            storage.setItem('accessToken', accessToken);
+                        });
+                }
+            })
     } else {
         $('button').remove();
         $('form')
@@ -35,5 +43,4 @@ $(document).ready(function () {
                 storage.setItem('handleTwo', newHandleTwo);
             });
     }
-
 })
