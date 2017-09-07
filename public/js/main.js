569e37e05116d8ba7b1af27db0b0dd37d111aa38
kaisinli <kaisinli12@gmail.com>
2017-09-06 23:57:39 -0400

    removed ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,25 @@
+var storage = window.localStorage;
+var accessToken = storage.getItem('accessToken');
+
 $(document).ready(function () {
     var handleOne = storage.getItem('handleOne');
     var handleTwo = storage.getItem('handleTwo');
     var accessToken = storage.getItem('accessToken');
 
-   if (accessToken === null){
-       $('body').addClass()
-   }
+    if (accessToken === null) {
+        $('#left-content').remove();
+        $('#right-content').remove();
+        $('body').addClass('not-authenticated');
+    } else if (handleTwo === null || handleOne === handleTwo) {
+        $('.intro-page').remove();
+        $('body').removeClass('not-authenticated');
+    } else {
+        $('.intro-page').remove();
+        $('body').removeClass('not-authenticated');
 
-})
+        $(function () {
+            
+
+        })
+    }
+});
