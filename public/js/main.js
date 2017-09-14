d6cd80ac0ea941aad5fc613af0cc87dc5ffdea66
kaisinli <kaisinli12@gmail.com>
2017-09-13 20:41:46 -0400

    waiting for IG Live mode

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+import { displayMul, displayOneOnLeft, displayOneOnRight } from './utils'
+
 var storage = window.localStorage;
 var accessToken = storage.getItem('accessToken');
 
@@ -10,23 +12,87 @@ $(document).ready(function () {
         $('#left-content').remove();
         $('#right-content').remove();
         $('body').addClass('not-authenticated');
-    } else if (handleTwo === null || handleOne === handleTwo) {
+    } else if (handleTwo === null || handleTwo === '' || handleOne === handleTwo) {
         $('.intro-page').remove();
         $('body').removeClass('not-authenticated');
+
+        $.ajax({
+            url: 'https://api.instagram.com/v1/users/search',
+            dataType: 'jsonp',
+            type: 'GET',
+            data: { access_token: accessToken, q: handleOne },
+
+            success: function (data) {
+                console.log('=== FOUND USER ID', data)
+                $.ajax({
+                    url: `https://api.instagram.com/v1/users/${data.data[0].id}/media/recent`,
+                    dataType: 'jsonp',
+                    type: 'GET',
+                    data: { access_token: accessToken, count: 2 },
+                    success: function (data) {
+                        console.log(data)
+                        var data = data.data;
+
+                        for (var i = 0; i < 2; i++) {
+                            displayMul(data, i)
+                        }
+                    },
+                    error: function (data) {
+                        console.log(data);
+                    }
+                });
+            },
+            error: function (data) {
+                console.log(data)
+            }
+        })
+
     } else {
         $('.intro-page').remove();
         $('body').removeClass('not-authenticated');
+
         $.ajax({
-            url: `https://api.instagram.com/v1/users/1461554942/media/recent`, 
+            url: 'https://api.instagram.com/v1/users/search',
             dataType: 'jsonp',
             type: 'GET',
-            data: { access_token: accessToken, count: 1 },
+            data: { access_token: accessToken, q: handleOne },
             success: function (data) {
-                console.log('success',data.data);
-            },
-            error: function (data) {
-                console.log(data); // send the error notifications to console
+                $.ajax({
+                    url: `https://api.instagram.com/v1/users/${data.data[0].id}/media/recent`,
+                    dataType: 'jsonp',
+                    type: 'GET',
+                    data: { access_token: accessToken, count: 1 },
+                    success: function (data) {
+                        var data = data.data;
+                        displayOneOnLeft(data, null)
+                    },
+                    error: function (data) {
+                        console.log(data)
+                    }
+                })
+            }
+        })
+
+        $.ajax({
+            url: 'https://api.instagram.com/v1/users/search',
+            dataType: 'jsonp',
+            type: 'GET',
+            data: { access_token: accessToken, q: handleTwo },
+            success: function (data) {
+                $.ajax({
+                    url: `https://api.instagram.com/v1/users/${data.data[0].id}/media/recent`,
+                    dataType: 'jsonp',
+                    type: 'GET',
+                    data: { access_token: accessToken, count: 1 },
+                    success: function (data) {
+                        var data = data.data;
+                        displayOneOnRight(data, null)
+                    },
+                    error: function (data) {
+                        console.log(data)
+                    }
+                })
             }
         })
     }
-});
+})
