48f76956e9b07a6f849f35a98ef35dd5eca3bd26
kaisinli <kaisieli12@gmail.com>
2017-11-09 14:17:44 -0500

    fixed

--- public/js/main.js
+++ public/js/main.js
@@ -14,57 +14,65 @@ $(document).ready(function () {
             .addClass('not-authenticated')
             .append('<div class="intro-page"><h1>Please click the Evertab icon to begin.</h1></div>');
     } else if (handle1 === null || handle1 === '' || handle1 === handle0) {
-        console.log('WHAT I WANT')
         $('.intro-page').remove();
         $('body').removeClass('not-authenticated');
-        $.getJSON(`https://www.instagram.com/${handle0}/media/`, function (data) {
-            console.log(data)
-            var content = data.items;
-
+        $.getJSON(`https://www.instagram.com/${handle0}/?__a=1`, function (data) {
+            var content = data.user.media.nodes;
+            var indexHolder = [];
             for (var i = 0; i < 2; i++) {
-                var igUrl = content[i].link;
-                var contentUrl = '';
 
-                if (content[i].videos) {
-                    contentUrl = content[i].videos.standard_resolution.url;
-                    console.log(i, contentUrl)
-                    $('#content' + i).attr('href', igUrl).append(`<video autoplay loop muted><source src = ${contentUrl} type="video/mp4" ></video>`)
+                var code = content[i].code;
+                var igUrl = `https://www.instagram.com/p/${code}`;
+                var contentUrl = '';
+                indexHolder.push(i);
+
+                if (content[i].is_video === true) {
+                    $.getJSON(`https://www.instagram.com/p/${code}/?__a=1`, function (data) {
+                        contentUrl = data.graphql.shortcode_media.video_url;
+                        $('#content' + indexHolder[0]).attr('href', igUrl).append(`<video autoplay loop muted><source src = ${contentUrl} type="video/mp4" ></video>`)
+                        indexHolder.shift()
+                    })
                 } else {
-                    contentUrl = content[i].images.standard_resolution.url;
-                    console.log(i, contentUrl)
+                    contentUrl = content[i].thumbnail_src;
                     $('#content' + i).attr('href', igUrl).append(`<img src = ${contentUrl}>`)
                 }
+                
             }
         })
     } else {
         $('.intro-page').remove();
         $('body').removeClass('not-authenticated');
 
-        $.getJSON(`https://www.instagram.com/${handle0}/media/`, function (data) {
-            var content = data.items
-            var igUrl = content[0].link;
-
+        $.getJSON(`https://www.instagram.com/${handle0}/?__a=1`, function (data) {
+            var content = data.user.media.nodes;
+            var code = content[0].code;
+            var igUrl = `https://www.instagram.com/p/${code}`;
             var contentUrl = ''
-            var ig
-            if (content[0].videos) {
-                contentUrl = content[0].videos.standard_resolution.url;
-                $('#content0').attr('href', igUrl).append(`<video autoplay loop muted><source src = ${contentUrl} type="video/mp4" ></video>`)
+
+            if (content[0].is_video === true) {
+                $.getJSON(`https://www.instagram.com/p/${code}/?__a=1`, function (data) {
+                    contentUrl = data.graphql.shortcode_media.video_url;
+                    $('#content0').attr('href', igUrl).append(`<video autoplay loop muted><source src = ${contentUrl} type="video/mp4" ></video>`)
+                })
             } else {
-                contentUrl = content[0].images.standard_resolution.url;
+                contentUrl = content[0].thumbnail_src;
                 $('#content0').attr('href', igUrl).append(`<img src = ${contentUrl}>`)
             }
         })
 
-        $.getJSON(`https://www.instagram.com/${handle1}/media/`, function (data) {
-            var content = data.items
+        $.getJSON(`https://www.instagram.com/${handle1}/?__a=1`, function (data) {
+            var content = data.user.media.nodes;
+            var code = content[0].code;
+            var igUrl = `https://www.instagram.com/p/${code}`;
             var contentUrl = ''
-            var igUrl = content[0].link;
 
-            if (content[0].videos) {
-                contentUrl = content[0].videos.standard_resolution.url;
-                $('#content1').attr('href', igUrl).append(`<video autoplay loop muted><source src = ${contentUrl} type="video/mp4" ></video>`)
+            if (content[0].is_video === true) {
+                $.getJSON(`https://www.instagram.com/p/${code}/?__a=1`, function (data) {
+                    contentUrl = data.graphql.shortcode_media.video_url;
+                    $('#content1').attr('href', igUrl).append(`<video autoplay loop muted><source src = ${contentUrl} type="video/mp4" ></video>`)
+                })
             } else {
-                contentUrl = content[0].images.standard_resolution.url;
+                contentUrl = content[0].thumbnail_src;
                 $('#content1').attr('href', igUrl).append(`<img src = ${contentUrl}>`)
             }
         })
